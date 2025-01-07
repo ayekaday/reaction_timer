0c51ef191c95a9d7e94103d5c28069d28baee7c0
ayekaday <yukiakd279@gmail.com>
2025-01-07 09:29:55 +0630

    add setInterval

--- src/components/GameBlock.vue
+++ src/components/GameBlock.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="block" v-if="showBlock">Click here {{ delay }}</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    Click me {{ score }}
+  </div>
 </template>
 
 <script>
@@ -8,23 +10,37 @@ export default {
   data() {
     return {
       showBlock: false,
+      score: 0,
+      timer: null,
     };
   },
   mounted() {
     setTimeout(() => {
       this.showBlock = true;
+      this.startTimer;
     }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.score += 50;
+      }, 50);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.score);
+    },
   },
 };
 </script>
 
 <style>
 .block {
-  background-color: greenyellow;
-  color: crimson;
-  margin: 100px auto;
+  background-color: darkgoldenrod;
+  color: cyan;
   width: 300px;
-  height: 100px;
+  height: 200px;
+  margin: 100px auto;
   padding: 20px;
 }
 </style>
